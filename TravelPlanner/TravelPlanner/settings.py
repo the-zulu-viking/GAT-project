22d5f534faf2ffec354fbfc18482d5655bbd3e53
the-zulu-viking <sven@getafricatravel.com>
2025-06-04 01:58:24 +0200

    created a custom 404

--- TravelPlanner/TravelPlanner/settings.py
+++ TravelPlanner/TravelPlanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m1-0wi)1(tm6_eah*hej(w#kw-^bj($6x5%yq@vqm*7!brf4!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
